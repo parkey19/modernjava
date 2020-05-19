1551ad0924872cd9ba0048a3cf32098c08e84349
Jin <kkttqq@gmail.com>
2020-05-19 17:34:20 +0900

    Update KMessageTest.java

--- src/test/java/com/parkey/modernjava/jsontest/KMessageTest.java
+++ src/test/java/com/parkey/modernjava/jsontest/KMessageTest.java
@@ -63,4 +63,27 @@ class KMessageTest {
 //        System.out.println(kMessage.getRow().getMap().get("id"));
 //        System.out.println(kMessage.getRow().getMap().get("groupKey"));
     }
+    
+    @Test
+    public void mapTest() throws IOException {
+        String json = "{\"schema\":\"yeogi\",\"row\":{\"after\":[{\"id\":16},{\"idType\":1},{\"channelId\":2},{\"agencyId\":1},{\"placeId\":1},{\"spaceId\":1},{\"groupKey\":\"aa\"},{\"targetId\":0},{\"deletedFlags\":0},{\"createdAt\":\"2020-03-03 10:51:10\"},{\"updatedAt\":\"2014-01-08 14:38:52\"}],\"before\":[{\"id\":16},{\"idType\":1},{\"channelId\":2},{\"agencyId\":1},{\"placeId\":1},{\"spaceId\":1},{\"groupKey\":\"bb\"},{\"targetId\":0},{\"deletedFlags\":0},{\"createdAt\":\"2020-03-03 10:51:10\"},{\"updatedAt\":\"2014-01-08 14:38:52\"}]},\"table\":\"policy\",\"type\":\"UpdateRowsEventV2\",\"@version\":\"1\",\"@timestamp\":\"2020-03-03T01:51:10.680Z\"}";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(json);
+        JsonNode row = jsonNode.get("row").get("after");
+
+        Optional<Map> collect = StreamSupport.stream(row.spliterator(), true)
+                .map(jsonNode1 -> mapper.convertValue(jsonNode1, Map.class))
+                .reduce(CompositeMap::new);
+
+        Map map = collect.get();
+
+        System.out.println(map.get("id"));
+        System.out.println(map.get("idType"));
+        System.out.println(map.get("channelId"));
+
+//         ModelMapper modelMapper = new ModelMapper();
+//         Policy Policy = modelMapper.map(map, Policy.class);
+//         System.out.println(Policy);
+
+    }
 }
